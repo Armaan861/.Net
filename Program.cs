71109edb79d221a3f810bf0d615503c5525b8a0b
Armaan Ramprakash Gupta <armaangupta613@gmail.com>
2026-07-06 18:44:24 +0530

    Add files via upload
    This is My Day 1 work

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Name :-Armaan Gupta");
+Console.WriteLine("Branch:- Computer Science Engineeering");
+Console.WriteLine("College:- SSGMCE");
+
+
+First f = new First();
+f.display();
