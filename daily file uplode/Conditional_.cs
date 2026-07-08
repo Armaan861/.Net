7d3e603e4bcb4dbde4f092cf27cf96c391d56585
Armaan Ramprakash Gupta <armaangupta613@gmail.com>
2026-07-08 17:25:59 +0530

    Day 2

---
+++ daily file uplode/Conditional_.cs
@@ -0,0 +1,17 @@
+
+int a = 90;
+
+
+
+if (a >= 90)
+{
+    Console.WriteLine(" Grade A");
+}
+else if (a >= 80)
+{
+    Console.WriteLine("Grade B");
+}
+else
+{
+    Console.WriteLine("Grade c");
+}
