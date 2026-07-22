26cc86c222ddfce1dda57f23a7ed443d3888b7c9
Armaan Ramprakash Gupta <armaangupta613@gmail.com>
2026-07-22 19:57:26 +0530

    22 july 2026 Visual Studio (2)

--- Daily File/HomeController.cs
+++ Daily File/HomeController.cs
@@ -1,6 +1,32 @@
-﻿namespace _21JUL2026
+using _22JULY_001_2026.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace _22JULY_001_2026.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
+        //display Form
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //recive form data
+        [HttpPost]
+        public ActionResult Index(Stationary stationary)
+        {
+            if (ModelState.IsValid)
+            {
+                //normally save to db
+                return Content($"StationaryName : {stationary.Name}, " + $" Price : {stationary.Price}," + $" Category : {stationary.Category}," + $" Stock : {stationary.Stock}");
+            }
+            else
+            {
+                return View(stationary);
+            }
+
+        }
     }
 }
