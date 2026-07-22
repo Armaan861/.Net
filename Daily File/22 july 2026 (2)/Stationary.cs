910811b0e1335cf187f402c8dd31ec2e051ddbaa
Armaan Ramprakash Gupta <armaangupta613@gmail.com>
2026-07-22 22:03:00 +0530

    22 july 2026 Visual Studio (2)

---
+++ Daily File/22 july 2026 (2)/Stationary.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _22JULY_001_2026.Models
+{
+    public class Stationary
+    {
+        public int Id;
+
+        [Required(ErrorMessage = "Name is mandatory")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is mandatory")]
+        [Range(1,10000)]
+        public decimal Price { get; set; }
+
+
+        [Required(ErrorMessage = "Category is mandatory")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Stock is mandatory")]
+        public int Stock { get; set; }
+    }
+}
