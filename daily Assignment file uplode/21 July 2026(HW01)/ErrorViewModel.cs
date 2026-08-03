2ddccb1a51ede7300f9adc263ce5ee29a36a11f9
Armaan Ramprakash Gupta <armaangupta613@gmail.com>
2026-08-04 00:06:28 +0530

    21 july 2026

--- daily Assignment file uplode/21 July 2026(HW01)/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace _21_July_2026_HW01.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
---
+++ daily Assignment file uplode/21 July 2026(HW01)/Models/Department.cs
@@ -0,0 +1,12 @@
+﻿namespace _21_July_2026_HW01.Models
+{
+    public class Department
+    {
+        public string DepartmentName { get; set; }
+        public string DepartmentHead { get; set; }
+
+        public long HeadContactNumber { get; set; }
+        public string email { get; set; }
+
+    }
+}
