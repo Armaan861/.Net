cb0754eb60a0bb84635c6dc3cbdfbd4e685b80d2
Armaan Ramprakash Gupta <armaangupta613@gmail.com>
2026-08-03 23:46:16 +0530

    28 july 2026

---
+++ Daily File/28  july/Controllers/Services/IProductServices.cs
@@ -0,0 +1,14 @@
+﻿using _28_july_2026.Controllers.Models;
+
+namespace _28_july_2026.Controllers.Services
+{
+    public interface IProductServices
+    {
+        List<Product> GetAll();
+        
+        Product? GetById(int id);
+        Product AddProduct(Product product);
+        Product? UpdateProduct(int id, Product product);
+        bool DeleteProduct(int id);
+    }
+}
