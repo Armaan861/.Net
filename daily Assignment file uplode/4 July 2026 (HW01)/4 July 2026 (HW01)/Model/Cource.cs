1992c6997eebc6674759a94be6f634cf58214453
Armaan Ramprakash Gupta <armaangupta613@gmail.com>
2026-08-07 18:33:48 +0530

    4 Aug 2026

---
+++ daily Assignment file uplode/4 July 2026 (HW01)/4 July 2026 (HW01)/Model/Cource.cs
@@ -0,0 +1,11 @@
+﻿namespace _4_July_2026__HW01_.Model
+{
+    public class Cource
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Duration { get; set; }
+        public int Credits { get; set; }
+
+    }
+}
