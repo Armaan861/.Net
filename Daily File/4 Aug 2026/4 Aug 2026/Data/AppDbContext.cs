c6c99101369ba7b1c54b0eefe90fbd5f5fbd9b6a
Armaan Ramprakash Gupta <armaangupta613@gmail.com>
2026-08-07 09:47:00 +0530

    4 Aug 2026

---
+++ Daily File/4 Aug 2026/4 Aug 2026/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using _4_Aug_2026.Models;
+using Microsoft.EntityFrameworkCore;
+namespace _4_Aug_2026.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
+
