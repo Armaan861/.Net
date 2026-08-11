05885f2fde928bf1042f3e0858ce166a9936b373
Armaan Ramprakash Gupta <armaangupta613@gmail.com>
2026-08-11 10:09:49 +0530

    8 Aug 2026

---
+++ daily Assignment file uplode/8 Aug 2026/8 Aug 2026/Data/AppDbContext.cs
@@ -0,0 +1,29 @@
+﻿using _8_Aug_2026.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace _8_Aug_2026.Data
+{
+    public class AppDbContext : DbContext
+    {
+        //constructor recive db configuration throughh DI
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //represent the student table i db
+        public DbSet<Student> Students => Set<Student>();
+
+
+        //represent the teacher table i db
+        public DbSet<Teacher> Teachers => Set<Teacher>();
+
+        //represent the course table i db
+        public DbSet<Course> Courses => Set<Course>();
+
+        //represent the Batch table i db
+        public DbSet<Batch> Batches => Set<Batch>();
+
+        
+    
+    }
+}
